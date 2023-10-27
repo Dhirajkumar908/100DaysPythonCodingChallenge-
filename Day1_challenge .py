6023985ace347dbde059c79c5f43a34e63e0c902
Dhiraj Kumar <kumardhirajkumar908@gmail.com>
2023-10-27 12:21:59 +0530

    Day 1 python coding challenge

---
+++ Day1_challenge .py
@@ -0,0 +1,15 @@
+#Write your full name and today's date in separate lines of code.
+print("Dhiraj Kumar")
+print("27/Oct/2023")
+#Copy this text below. See if you can do it with just the one print statement!
+
+print("""
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+""")
+print("Iam feeling :)")
+
+print("You can follow my progress at replit.com/@kumardhirajkuma")
+
+#replit100DaysfCode
