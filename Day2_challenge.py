4ab9e0f70090845f0405cbf1eca1df6e759786c0
Dhiraj Kumar <kumardhirajkumar908@gmail.com>
2023-10-28 11:23:08 +0530

    Day2 python codin challenge

---
+++ Day2_challenge.py
@@ -0,0 +1,20 @@
+#Asks for the user's name, favorite food, favorite music and where they live (or you can create other questions!)
+#Store all of the answers in different variables.
+#Print out a full sentence that includes the user's favorite things.
+#Give them a positive affirmation at the end - tell them they rock at doing something!Example (go
+
+username=input("Enter your username:")
+favorite_food=input("what is you favorite food?")
+favorite_music=input("what is your favorite music?")
+add=input("where do you leav")
+print()
+print("Getting to Know you!")
+print("You are")
+print(username)
+print("You're probably hungry for")
+print(favorite_food)
+print("and you're definitely getting your groove on to synthwave and anything by our man rick astleyliving in the amazing city of ")
+print (add)
+
+
+
