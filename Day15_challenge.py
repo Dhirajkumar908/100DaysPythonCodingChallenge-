15e150e6d8a23a7fdd66eee155f0082d36c7bf01
Dhiraj Kumar <kumardhirajkumar908@gmail.com>
2023-11-15 12:46:30 +0530

    Day15 Python coding challenge

---
+++ Day15_challenge.py
@@ -0,0 +1,20 @@
+"""
+All About Loops
+
+Day 15 Challenge
+Write a program that loops. Inside the loop, ask the user what animal sound they want to hear.
+"""
+
+exit = "no"
+
+while exit == "no":
+    animal = input("what animal do you want?:")
+    if animal == "Cow" or animal == "cow":
+        print("A cow goes mooo....")
+    elif animal == "Dog" or animal == "dog":
+        print('A doc goes bhau bhau bhau')
+    elif animal == "Cat" or animal == "cat":
+        print("a cat say meow meow")
+    else:
+        print("You are not animal Lover")
+    exit = input("You want to Exit?:")
