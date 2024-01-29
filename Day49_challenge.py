de4778813b3fa85c69417591e1d999286633e7f8
Dhiraj Kumar <kumardhirajkumar908@gmail.com>
2024-01-29 10:29:39 +0530

    Day49 python coding challege

---
+++ Day49_challenge.py
@@ -0,0 +1,13 @@
+f = open("filename.txt", "r")
+scores = f.read().split("\n")
+f.close()
+
+highscore = 0
+name = None
+for rows in scores:
+  data = rows.split()
+  if data != []:
+    if int(data[1]) > highscore:
+      highscore = int(data[1])
+      name = data[0]
+print("the winner is", name, "with", highscore)
